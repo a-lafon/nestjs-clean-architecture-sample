4ea9ffcfc1b32c0815a1a0cb20454c374ea72a6b
Arnaud Lafon <arnaud.lafon@proton.me>
2023-05-15 18:14:23 +0200

    feat(domain): create entities

---
+++ src/domain/entities/todo.entity.ts
@@ -0,0 +1,8 @@
+export class Todo {
+  public id: number;
+  public title: string;
+  public description: string;
+  public isCompleted: boolean;
+  public createdAt: Date;
+  public updatedAt: Date;
+}
---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  public id: number;
+  public username: string;
+  public email: string;
+  public password: string;
+  public createdAt: Date;
+  public updatedAt: Date;
+}
